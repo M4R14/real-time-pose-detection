523acf34768c98cc52e76c3534edc4eb35080f58
eakarin <eakarin@eakarins-MacBook-Pro.local>
2020-02-23 13:59:31 +0700

    start code

---
+++ camera.js
@@ -0,0 +1,59 @@
+const video = document.querySelector('video');
+    const display = document.querySelector('canvas');
+
+    const img = document.createElement('img');
+    const canvas = document.createElement('canvas');
+
+    const constraints = {
+        video: {
+            width: 640,
+            height: 480
+        }
+    };
+
+    navigator.mediaDevices.getUserMedia(constraints)
+        .then((stream) => {video.srcObject = stream});
+
+    const marker = (keypoints, ctx) => {
+        keypoints.forEach(point => {
+            const { position, part } = point;
+            // ctx.font = "12px Arial";
+            ctx.fillText(part, position.x + 10, position.y + 5);
+            ctx.fillRect(position.x, position.y, 5, 5);
+        })
+    }
+
+    const estimateMultiplePoses = () => {
+        posenet.load({
+            architecture: 'MobileNetV1',
+            outputStride: 16,
+            inputResolution: { width: 640, height: 480 },
+            multiplier: 0.75
+        })
+        .then(function(net){
+            console.log('estimateMultiplePoses .... ');
+            return net.estimateMultiplePoses(img, {
+                flipHorizontal: false,
+                maxDetections: 2,
+                scoreThreshold: 0.6,
+                nmsRadius: 20})
+        }).then(function(poses){
+            console.log('got Poses .... ', poses);
+            display.width = img.width;
+            display.height = img.height;
+            var ctx = display.getContext("2d");
+            ctx.fillStyle = "#FF0000";
+
+            poses.forEach(element => marker(element.keypoints, ctx));
+        })
+    }
+
+    setInterval(() => {
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
+        canvas.getContext('2d').drawImage(video, 0, 0);
+
+        const dataImge = canvas.toDataURL('image/webp');
+        img.src = dataImge;
+        estimateMultiplePoses();
+    }, 100)
